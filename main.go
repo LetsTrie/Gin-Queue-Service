1d7f9531f91c38eaf903f8500211cbee837cae23
letstrie <sakibkhan111296@gmail.com>
2023-02-05 02:44:20 +0600

    Load Env Vars

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"server/controllers"
+	"server/initializers"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	initializers.LoadEnvVariables()
+}
+
 func main() {
+	// Initialize Gin router
 	r := gin.Default()
 	
+	// Define route handlers
 	r.GET("/tasks", controllers.GetTasks)
 	r.GET("/tasks/:id", controllers.GetTask)
 	r.POST("/tasks", controllers.CreateTask)
 	
-	r.Run(":8080")
+	// Start server
+	r.Run()
 }
